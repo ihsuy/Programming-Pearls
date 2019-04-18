c2f9dc5203e6af50c8727a7a4b33536ab1a6492d
Yushi Qiu <jooehisaishi@gmail.com>
2019-04-18 18:06:37 +0900

    method1 using reverse is uploaded

--- column2_Aha_Algorithms/3_vectorRotation.cpp
+++ column2_Aha_Algorithms/3_vectorRotation.cpp
@@ -31,13 +31,36 @@ inline void inspect(T& t){typename T::iterator i1 = t.begin(), i2 = t.end(); whi
 using namespace std;
 
 /*
-
+We skimmed two vector rotation algorithms 
+that require subtle code; implement each as 
+a program. How does the greatest common divisor 
+of i and n appear in each program?
 */
+
+// method 1 do 3 reverse
+// this method can be understood if you look at an example
+// let nums = 1,2,3,4,5,6 and we want to 
+// rotate it to the right by 3 steps
+// original: 1 2 3 4 5 6
+// rotated : 4 5 6 1 2 3
+// notice if we reverse the first half and the 
+// second half of the rotated result
+// we get    6 5 4 3 2 1
+// which is what we get reversing the original vector
+void vector_rotate1(vector<int>& nums, int n)
+{	// rotate nums to the right by n
+	n %= nums.size();
+	reverse(nums.begin(), nums.end());
+	reverse(nums.begin(), nums.begin()+n);
+	reverse(nums.begin()+n, nums.end());
+}
 
 
 int main()
 {
-	
+	vector<int> nums {1,2,3,4,5,6,7,8};
+	vector_rotate1(nums, 3);
+	inspect<vector<int>>(nums);
 
 	return 0;
 }
