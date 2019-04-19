6039248f360ef2f2467be3386b81a1e535590411
Yushi Qiu <jooehisaishi@gmail.com>
2019-04-19 15:55:27 +0900

    reorganize code format

--- column2_Aha_Algorithms/9_SequentialSearch_and_BinarySearch_tradeoff.cpp
+++ column2_Aha_Algorithms/9_SequentialSearch_and_BinarySearch_tradeoff.cpp
@@ -80,47 +80,56 @@ int main()
 {
 	const size_t kNumsSize = 100000;
 	const size_t test_case_number = 50000;
-	size_t checkpoint = test_case_number/5;
+	size_t checkpoint = test_case_number / 5;
+	
 	// generate kNumsSize unique random numbers
 	vector<int> nums;
 	nums.reserve(kNumsSize);
 
 	cout << "generating " << kNumsSize << " unique random integers...\n";
-	for(int i = 0; i < kNumsSize; ++i)
+
+	for (int i = 0; i < kNumsSize; ++i)
 	{
 		nums.push_back(i);
 	}
+
 	srand(chrono::high_resolution_clock::now().time_since_epoch().count());
-	for(int i = 0; i < kNumsSize; ++i)
+
+	for (int i = 0; i < kNumsSize; ++i)
 	{
-		swap(nums[i], nums[i + rand()%(kNumsSize-i)]);
+		swap(nums[i], nums[i + rand() % (kNumsSize - i)]);
 	}
 	vector<int> nums_for_seqSearch = nums;
+
 	cout << "completed\n";
 
 	cout << "preprocessing for binarySearch...\n";
+
 	auto t1 = chrono::high_resolution_clock::now();
 	sort(nums.begin(), nums.end());
-	auto t2 = chrono::high_resolution_clock::now()-t1;
+	auto t2 = chrono::high_resolution_clock::now() - t1;
 	auto sorting_time_in_nanoseconds = chrono::duration_cast<chrono::nanoseconds>(t2).count();
+
 	cout << "completed\nsorting time spent: " << sorting_time_in_nanoseconds << " nanoseconds\n";
 
 	cout << "comparing performance of BinarySearch and SequentialSearch for " << test_case_number << " times\n";
+
 	long long total_sequential_search_time = 0;
 	long long total_binary_search_time = 0;
 
 	long long total_time_difference = 0;
 	long long turning_point = -1; // number of search need to perform to make binary search cheaper
-	for(int i = 0; i < test_case_number; ++i)
+
+	for (int i = 0; i < test_case_number; ++i)
 	{
-		if(i%checkpoint==0)
+		if (i % checkpoint == 0)
 		{
-			cout << "processing... " << ((double)i/test_case_number)*100 << "%\n";
+			cout << "processing... " << ((double)i / test_case_number) * 100 << "%\n";
 		}
 		auto begin = chrono::high_resolution_clock::now();
 		SequentialSearch(nums_for_seqSearch, i);
 		auto time_elapsed = chrono::high_resolution_clock::now() - begin;
-		
+
 		auto sequential_search_time = chrono::duration_cast<chrono::nanoseconds>(time_elapsed).count();
 
 		begin = chrono::high_resolution_clock::now();
@@ -138,11 +147,12 @@ int main()
 		// cout << "difference            : " << time_difference << " nanoseconds\n";
 		total_time_difference += time_difference;
 
-		if(turning_point == -1 and total_time_difference > sorting_time_in_nanoseconds)
+		if (turning_point == -1 and total_time_difference > sorting_time_in_nanoseconds)
 		{
 			turning_point = i;
 		}
 	}
+
 	cout << "processing... 100%\nComplete\n";
 
 	cout << "total_sequential_search_time: " << total_sequential_search_time << " nanoseconds\n";
@@ -150,5 +160,6 @@ int main()
 	cout << "total_time_difference       : " << total_time_difference << " nanoseconds\n";
 	cout << "number binary searches need be performed to buy back the preprocessing time:\n";
 	cout <<	turning_point << '\n';
+
 	return 0;
 }
