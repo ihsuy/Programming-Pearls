479a12df5590d553dd06acd15e3fc18aaec1ed43
Yushi Qiu <jooehisaishi@gmail.com>
2019-04-20 22:44:47 +0900

    quick update

--- column3_Data_Structures_Programs/2_kthOrderLinearRecurrence.cpp
+++ column3_Data_Structures_Programs/2_kthOrderLinearRecurrence.cpp
@@ -41,21 +41,72 @@ that program compared to a program that evaluates one particular
 fifth-order recurrence, but does so without using arrays?
 */
 
-// can use array
-vector<int> kthLinearRecurrence(const vector<int>& a, const vector<int>& c,
-                                const int& k, const int& m)
-{
-	if(m <= k)
+double kthLinearRecurrence(const vector<double>& a, const vector<double>& c,
+                           const double& k, const double& m)
+{	// note this program doesn't do exactly what the problem asks
+	// instead of returning the ouput a_1 through a_m
+	// i just return am.
+	// since if m is less than or equal to k, the result is already provided
+	// I dont see the point why I have to fill those information into a
+	// different container again
+	// only a_m will be the new information (if m is k+1)
+	if (m <= k)
 	{
-		return {a.begin(), a.begin()+m};
+		return a[m];
 	}
+	else if (m == k + 1)
+	{
+		double result = 0.0;
+		for (int i = 0; i < a.size(); ++i)
+		{
+			result += c[i] * a[a.size() - 1 - i];
+		}
+		return result + c.back();
+	}
+	else
+	{
+		throw runtime_error("no enough information about c to produce the result");
+	}
+}
 
-	
+// a program that calculates the fifth order recurrence without using array
+double fifthOcurrence(const double& a1, const double& a2,
+                      const double& a3, const double& a4,
+                      const double& c1, const double& c2,
+                      const double& c3, const double& c4,
+                      const double& c5)
+{
+	return c1 * a4 + c2 * a3 + c3 * a2 + c4 * a1 + c5;
+}
+// the program above may looks simple(??)
+// but its actually horribly unfriendly to its users
+// imagine when you got this function and have to insert all nine parameters
+// using your hand like this:
+// fifthOccurence(12.24, 219.12, 9.89, 82.48, 12.24, 219.12, 55.9, 2.8, 19.24)
+// do you still remember where begins your first c1 ?
+// ok if you say you will use named variables but then you will need 9 new lines
+// just initialize your variables...
+// also what if your boss want you to make it a function that calculates
+// the 100th ocurrence? you will have 201 variables to create then
+
+// below is a function does the job calculating occurence of any order using array
+double nthLinearRecurrence(const vector<double>& a, const vector<double>& c)
+{
+	assert(a.size() == (c.size()-1));
+
+	double result = 0.0;
+	for (int i = 0; i < a.size(); ++i)
+	{
+		result += c[i] * a[a.size() - 1 - i];
+	}
+	return result + c.back();
 }
 
 int main()
 {
-
+	vector<double> a {1, 5, 6, 7, 12, 3};
+	vector<double> c {2, 5, 1, 1, 11, 1, 4};
+	cout << kthLinearRecurrence(a, c, 6, 7) << endl;
 
 	return 0;
 }
