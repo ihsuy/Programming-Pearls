b27d9ee1641d9fe984e530f89072e13fc54b15eb
Yushi Qiu <jooehisaishi@gmail.com>
2019-04-23 11:12:39 +0900

    added analysis

--- column4_Writting_Correct_Programs/6_coffee_can_problem.cpp
+++ column4_Writting_Correct_Programs/6_coffee_can_problem.cpp
@@ -60,12 +60,12 @@ bool CoffeeCanProblem_simulation(int nBlack_bean, int nWhite_bean)
         bool isBlack1 = ((rand() % bean_pool_size) < nBlack_bean);
         bool isBlack2 = ((rand() % bean_pool_size) < nBlack_bean);
 
-        if(nWhite_bean < 2 and not isBlack1 and not isBlack2)
+        if (nWhite_bean < 2 and not isBlack1 and not isBlack2)
         {   // when there're fewer than 2 white beans left
             // we can't draw 2 white beans, so one of them has
             // to be black
             isBlack1 = true;
-            if(nWhite_bean == 0)
+            if (nWhite_bean == 0)
             {   // if there was no white beans at all
                 // then we can only choose black beans
                 // Note: we know that we have at least 2 black beans left
@@ -73,10 +73,10 @@ bool CoffeeCanProblem_simulation(int nBlack_bean, int nWhite_bean)
                 isBlack2 = true;
             }
         }
-        else if(nBlack_bean < 2 and isBlack1 and isBlack2)
+        else if (nBlack_bean < 2 and isBlack1 and isBlack2)
         {   // similar to above
             isBlack1 = false;
-            if(nBlack_bean == 0)
+            if (nBlack_bean == 0)
             {
                 isBlack2 = false;
             }
@@ -107,13 +107,71 @@ bool CoffeeCanProblem_simulation(int nBlack_bean, int nWhite_bean)
     cout << "black: " << nBlack_bean << " white: " << nWhite_bean << '\n';
     return nBlack_bean == 1;
 }
+// Review of the process:
+// Randomly select two beans from the can. If they are the same
+// color, throw them both out and insert an extra black bean.
+// If they are different colors, return the white bean to the
+// can and throw out the black.
 
+// Note the termination will happen when there's only 1 bean left in the can
+// that means, we want to carefully inspect how this process throw out our beans
 
+// First, let's look at what happens to white beans.
+// note that there're only 1 cases when white beans are removed,
+// namely, when we draw 2 white beans together. this will cause both of them
+// to be removed. This is a huge hint, since the number of white beans can only
+// decrease by even number, if we had an odd number of white beans in the can
+// then we will absolutely have at least 1 white bean left doesn't
+// matters what happens.
+
+// Next, let's look at Black beans. Notice that, black beans are always removed
+// except when 2 white beans are drew together which, however, will add 1 black
+// bean back. in all other cases i.e. When we draw 2 black beans or
+// 1 white 1 black, in total, only 1 black bean will be removed.
+// note that, there are only 3 possiblilties for drawing 2 beans
+// (white, black) (white, white) (black, black), all of them will result in
+// a decrease of the total number of beans in the can by 1
+
+// now we know,
+// 1. each step in the process will decrease number of black beans by at most 1
+// 2. if we are to throw out white beans, we always throw 2 together
+// 3. every step (if there're enough beans left),
+//    total number of beans in the can will decrease by 1
+
+// after knowing 3, we are confident that total number of beans left will
+// always be reduced to 0 or 1 (if we don't have 2 beans left at the end).
+// and now we prove it will only be reduced to 1
+
+// since to remove white beans, we don't need any information about black beans
+// but only to know we had at least 2 white beans left,
+// we can isolate this process:
+// for initial number of white beans in the can, it can only be either odd or
+// even
+// when its odd, the number of white beans will be reduced to 1 and no further
+// when its even, the number of white beans will be reduced to 0
+// note: both of the cases produce nWhite_beans/2 black beans
+
+// for black beans
+// when total number of black beans in the can (initial + later produced) is
+// odd or even(non zero), we are always left with 1 black beans
+// because when current number of black beans is even, we do nBlack_bean/2
+// and otherwise nBlack_bean/2 +1, this will reduce any positive integer to 1
+
+// so now we know this process will always reach at the point
+// 1 black bean, 0 or 1 white bean
+// i.e. case 1(black, white) or case2(black, none)
+// the first case will yield after one step (none, white) and terminates
+// and the second case terminates directly
+// therefore this process will always terminate
+
+// 1. (black, none) 2. (none, white)
+// 1 will happen whenever number of white is even (include 0)
+// 2 will happen whenever number of white is odd
 int main()
 {
     srand(chrono::high_resolution_clock::now().time_since_epoch().count());
 
-    CoffeeCanProblem_simulation(101, 101);
+    CoffeeCanProblem_simulation(56, 7);
 
     return 0;
 }
