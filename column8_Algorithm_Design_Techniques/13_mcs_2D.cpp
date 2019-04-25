62f6cdcdd9883668a4c9b79288f569a326d93357
Yushi Qiu <jooehisaishi@gmail.com>
2019-04-25 17:03:18 +0900

    added n3 solution and explaination

--- column8_Algorithm_Design_Techniques/13_mcs_2D.cpp
+++ column8_Algorithm_Design_Techniques/13_mcs_2D.cpp
@@ -55,7 +55,7 @@ ostream& operator<<(ostream& os, const window& w)
     return os << out;
 }
 
-// naive solution
+// naive brute force solution
 // formidable 6 for loops
 window maxWindow(const vector<vector<int>>& m)
 {
@@ -89,17 +89,118 @@ window maxWindow(const vector<vector<int>>& m)
     return max_window;
 }
 
+// improved brute force solution
+// doesn't re-calculate the window sum from scratch
+// but subtract the removed part and add the new part of the window
+window maxWindow2(const vector<vector<int>>& m)
+{
+    int n_row = m.size(), n_col = m[0].size();
+    window max_window;
+    // for every possible window size
+    for (int r = 1; r <= n_row; ++r)
+    {
+        for (int c = 1; c <= n_col; ++c)
+        {   // move window to every possible position
+            // calculate initial sum of the window
+            int window_sum = 0;
+            for (int i = 0; i < r; ++i)
+            {
+                for (int j = 0; j < c; ++j)
+                {
+                    window_sum += m[i][j];
+                }
+            }
+
+            for (int wr = 0; wr < n_row - r + 1; ++wr)
+            {
+                int pre_window = window_sum;
+
+                for (int wc = 0; wc < n_col - c + 1; ++wc)
+                {
+                    if (window_sum > max_window.sum)
+                    {
+                        max_window.update({wr, wc}, r, c, window_sum);
+                    }
+                    if (wc != n_col - c)
+                    {   // substract first column from window
+                        // (how: iterate vertically alone rows)
+                        int col_remove = 0;
+                        for (int i = 0; i < r; ++i)
+                        {
+                            col_remove += m[wr + i][wc];
+                        }
+                        window_sum -= col_remove;
+                        // add next column of window
+                        int col_add = 0;
+                        for (int i = 0; i < r; ++i)
+                        {
+                            col_add += m[wr + i][wc + c];
+                        }
+                        window_sum += col_add;
+                    }
+                }
+                // reset window
+                window_sum = pre_window;
+
+                if (wr != n_row - r)
+                {   // substract first row from the initial window
+                    int row_remove = 0;
+                    for (int i = 0; i < c; ++i)
+                    {
+                        row_remove += m[wr][i];
+                    }
+                    window_sum -= row_remove;
+
+                    // add the new row to the initial window
+                    int row_add = 0;
+                    for (int i = 0; i < c; ++i)
+                    {
+                        row_add += m[wr + r][i];
+                    }
+                    window_sum += row_add;
+                }
+            }
+        }
+    }
+    return max_window;
+}
+
+vector<vector<int>> TestCaseGenerator(const int& h, const int& w, const int& n)
+{
+    vector<vector<int>> test_case;
+    for (int r = 0; r < h; ++r)
+    {
+        vector<int> row;
+        for (int c = 0; c < w; ++c)
+        {
+            row.push_back(rand()%n - n/2);
+        }
+        test_case.push_back(row);
+    }
+    return test_case;
+}
+
+void run(window(f)(const vector<vector<int>>&), const vector<vector<int>>& m)
+{
+    auto t1 = chrono::high_resolution_clock::now();
+    auto res = f(m);
+    auto t2 = chrono::high_resolution_clock::now() - t1;
+    auto t = chrono::duration_cast<chrono::microseconds>(t2).count();
+    cout << "------------\n";
+    cout << "result: " << res << '\n';
+    printf("time spent: %lld microseconds\n", t);
+    cout << "------------\n";
+}
+
 int main()
 {
-    vector<vector<int>> m
-    {
-        { -1,  1,  2,  3},
-        { 2, -2,  3, -1},
-        { 0,  2,  1,  1},
-        { 1,  2, -3,  -4}
-    };
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    const int h = 50;
+    const int w = 50;
+    const int n = 100;
+    auto m = TestCaseGenerator(h, w, n);
 
-    cout << maxWindow(m) << '\n';
-
+    run(maxWindow, m);
+    run(maxWindow2, m);
     return 0;
 }
