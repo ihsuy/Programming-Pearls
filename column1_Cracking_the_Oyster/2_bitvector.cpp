d91b14f2b2bf7d62cc7c7013a9dadc60da5a2b8b
Yushi Qiu <jooehisaishi@gmail.com>
2019-04-26 09:35:43 +0900

    update code template

--- column1_Cracking_the_Oyster/2_bitvector.cpp
+++ column1_Cracking_the_Oyster/2_bitvector.cpp
@@ -17,12 +17,6 @@
 #include <string>
 
 typedef long long ll;
-inline int two(int n) { return 1 << n; }
-inline int test(int n, int b) { return (n >> b) & 1; }
-inline void set_bit(int & n, int b) { n |= two(b); }
-inline void unset_bit(int & n, int b) { n &= ~two(b); }
-inline int last_bit(int n) { return n & (-n); }
-inline int ones(int n) { int res = 0; while (n && ++res) n -= n & (-n); return res; }
 template<typename T>
 inline void inspect(T& t) {typename T::iterator i1 = t.begin(), i2 = t.end(); while (i1 != i2) {std::cout << (*i1) << ' '; i1++;} std::cout << '\n';}
 
---
+++ column8_Algorithm_Design_Techniques/t.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <list>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <utility>
+#include <bitset>
+#include <fstream>
+#include <string>
+
+typedef long long ll;
+template<typename T>
+inline void inspect(T& t){typename T::iterator i1 = t.begin(), i2 = t.end(); while (i1 != i2) {std::cout << (*i1) << ' '; i1++;} std::cout << '\n';}
+
+/////////////////////////////////////////////////////////////
+using namespace std;
+
+/*
+
+*/
+
+
+
+int main()
+{
+
+
+	return 0;
+}
