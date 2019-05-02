83b817080cd3f5a11f795b9c6c9678b6cc69d1cd
Yushi Qiu <jooehisaishi@gmail.com>
2019-05-02 10:55:36 +0900

    added comments and explanations

--- column12_A_Sample_Problem/9_optimal_random_generator.cpp
+++ column12_A_Sample_Problem/9_optimal_random_generator.cpp
@@ -42,8 +42,30 @@ vector<int> RandomSet_naive(const int& m, const int& n)
     return {s.begin(), s.end()};
 }
 
-// I understand why this code runs
+// First let's define the denotation. Let
+// rand{a,b} be a random variable following 
+// the discrete uniform distribution over the set {a, a+1, ..., b-1, b}.
+// This algorithm can be better understood if an example is provided
+// let m = 5 and n = 10, we know 
+// j ∈ [5,6,7,8,9]
+// and thus t ∈ [rand{0,5}, rand{0,6}, rand{0,7}, rand{0,8}, {0,9}]
+// Let's look at what happens in every step
+// First, when the set s is empty, whatever value t is chosen from {0,5},
+// will be inserted in to s. 
+// Why? isn't this a bias toward certain set of numbers?
+// Not really, because we only have 10 numbers, and if we don't choose any
+// number from 0-5, we are only left with 4 available numbers!
+// it's impossible to form a subset of size 5.
+// Thus we choose the first element with probability equal to 1/5
+// the first step was legit, let's continue.
 
+// Next will happen is that we're getting the number rand{0, 6} when j = 6
+// now note that we have 2 chances to pick 6, namely when we select the number
+// that we selected previously, or we select 6 itself.
+// and for 7 we have 3 chances, and 6 also get another chance...
+
+// After carefully examining the whole process, its not hard to find that
+// every number had 5 chances to be selected. 
 vector<int> RandomSet_optimal(const int& m, const int& n)
 {
     set<int> s;
